0a56af3c537f7e8d53b088cac62b8183ff797996
Franck Theeten <franck.theeten@africamuseum.be>
2020-02-08 20:19:07 +0100

    Able to generate frequency and derivate plots

--- eod_class.R
+++ eod_class.R
@@ -58,7 +58,16 @@ Eod <-setRefClass("Eod",
                        chosen_baseline_index="numeric",
                        chosen_baseline="ANY",
                        chosen_baseline_position="numeric",
-                       chosen_last_position="numeric"
+                       chosen_last_position="numeric",
+                       normalized_wave= "ANY", 
+                       v_frame_zero = "ANY",
+                       valleys_peaks = "ANY",
+                       padded_normalized_wave_for_periodgram = "ANY",
+                       periodgram_frame = "ANY",
+                       periodgram = "ANY",
+                       main_plot="ANY",
+                       periodgram_plot = "ANY",
+                       derivate_plot = "ANY"
                      )
                      
 )
@@ -178,7 +187,7 @@ Eod$methods(
       }
       colnames(file_wave) <<- c("time", "amplitude")
     },
-  
+    #geters and setters
     getMetadata=function()
     {
       file_metadata
@@ -188,7 +197,16 @@ Eod$methods(
     {
      file_wave
     },
-  
+    getMainPlot = function()
+    {
+      main_plot
+    }
+    ,
+    getDerivatePlot = function()
+    {
+      derivate_plot
+    },
+       #sc functions
     defineBaseline=function()
     {
       
@@ -278,30 +296,281 @@ Eod$methods(
       chooseBaselineAndNormalize("meanvar")
       frame_baseline
     },
+    #main sc function
     chooseBaselineAndNormalize = function(type) #type = "mean, meanvar, var, arbitrary
     {
+      print("normalize")
+      print(type)
       if(type=="mean")
       {
-        chosen_baseline_position <<- 1
+        chosen_baseline_index <<- 1
       }
       else if(type=="var")
       {
-        chosen_baseline_position <<- 2
+        chosen_baseline_index <<- 2
       }
       else if(type=="meanvar")
       {
-        chosen_baseline_position <<- 3
+        chosen_baseline_index <<- 3
       }
       else if(type=="arbitrary")
       {
-        chosen_baseline_position <<-4
+        chosen_baseline_index <<-4
       }
+      print(chosen_baseline_index)
       chosen_baseline<<-frame_baseline$mean[chosen_baseline_index]
       chosen_baseline_position<<-frame_baseline$first_position[chosen_baseline_index]
       chosen_last_position<<-frame_baseline$last_position[chosen_baseline_index]
       
-    }
+      #normalize amplitude
+      normalized_wave <<- data.frame(file_wave)
+
+      normalized_wave$amplitude <<- normalized_wave$amplitude - chosen_baseline
+      
+      normalized_wave$amplitude <<- normalized_wave$amplitude/(max(normalized_wave$amplitude) - min(normalized_wave$amplitude))
+      View(normalized_wave)
+      createPeriodgram()
+      
+      
+      signal_limit = start_end_signal()
+      print("SIGNAL LIMIT")
+      print(signal_limit)
+      v_start_time_signal = normalized_wave$time[signal_limit[1]]
+      print(v_start_time_signal)
+      v_end_time_signal = normalized_wave$time[signal_limit[2]]
+      print(v_end_time_signal)
+      print("signal duration=")
+      print(v_end_time_signal - v_start_time_signal)
+      v_frame_start_end  = normalized_wave[signal_limit, ]
+      v_frame_start_end$labels = c("T1","T2")
+      
+      v_zero_point<-which(abs(normalized_wave$time)==min(abs(normalized_wave$time)))
+      v_frame_zero <<- normalized_wave[v_zero_point,]
+      
+      valleys_peaks <<- find_peaks_and_valleys(normalized_wave,chosen_baseline_position,chosen_last_position-1, p_nb_peaks=maxPeaks )
+      print(valleys_peaks)
+      #main plot
+      main_plot <<- save_plot(normalized_wave, "Wave and landmarks (normalized)", valleys_peaks, v_frame_zero, v_frame_start_end, chosen_baseline_position )
+      
+      #deriv plot
+      tmp_frame_deriv<-fct_diff_deriv(normalized_wave)
+      v_frame_deriv_start_end  <- tmp_frame_deriv[signal_limit, ]
+      v_frame_deriv_start_end$labels <- c("T1","T2")
+      v_frame_deriv_zero <- tmp_frame_deriv[v_zero_point,]
+      derivate_plot <<- save_plot(tmp_frame_deriv, "Derivate", valleys_peaks, v_frame_deriv_zero, v_frame_deriv_start_end, chosen_baseline_position )
+    },
+  
+  start_end_signal = function()
+  {
     
+    tmp_amplitude <- abs(normalized_wave$amplitude)
+    signal <- which(tmp_amplitude > threshold)
+    c(min(signal), max(signal))
+  },
+  peak_detection = function(p_frame, start_index,last_position, p_threshold=NULL, diff_threshold=NULL)
+  {
+    v_position=c()
+    v_prominence=c()
+    previous_peak=0
+    #v_prominence <-c()
+    for(i in start_index:last_position )
+    {
+      p_val=p_frame$amplitude[i]
+      p_previous=p_frame$amplitude[i-1]
+      p_next=p_frame$amplitude[i+1]
+      if(p_val > p_previous && p_val == p_next)
+      {
+        i<-detect_peak_plateau(p_frame, i, nrow(p_frame)-1)
+        p_next=p_frame$amplitude[i+1]
+      }
+      if(p_val > p_previous && p_val > p_next )
+      {
+        if(!is.null(p_threshold))
+        {
+          if(p_val<=p_threshold)
+          {
+            next
+          }
+        }
+        if(!is.null(diff_threshold))
+        {
+          if((p_val-p_previous )<=diff_threshold)
+          {
+            next
+          }
+        }
+        
+        v_prominence<-c(v_prominence, abs(p_val-previous_peak)) 
+        v_position=c(v_position, i)
+        previous_peak=p_val
+      }
+      
+    }
+    v_returned<-data.frame(position=v_position, prominence=v_prominence)
+    v_returned
+  },
+  
+  find_peaks_and_valleys = function( p_frame, p_silent_index, p_last_position, p_diff_threshold=NULL, p_nb_peaks=global_nb_peaks)
+  {
+    
+   
+    o_peaks<-peak_detection(p_frame, p_silent_index,p_last_position, diff_threshold =p_diff_threshold )
+    p_frame$amplitude=(p_frame$amplitude) * - 1
+    o_peaks2<-peak_detection(p_frame, p_silent_index,p_last_position, p_threshold=0,diff_threshold =p_diff_threshold)
+   
+    v_all_submits=rbind(o_peaks, o_peaks2)
+    v_all_submits<-v_all_submits[order(v_all_submits$prominence),]
+   
+    v_peaks=v_all_submits$position
+    
+    v_peaks<-unique(v_peaks)
+    
+    v_peaks<-tail(v_peaks, n=p_nb_peaks)
+    
+    v_peaks
+  }
+  ,
+  detect_peak_plateau = function(p_frame, current_position, last_position)
+  {
+    returned<-current_position
+    p_ref_val=p_frame$amplitude[current_position]
+    for(i in current_position+1:last_position)
+    {
+      p_val<-p_frame$amplitude[i]
+      if(p_val!=p_ref_val)
+      {
+        return(i-1)
+      }
+    }
+    returned
+  },
+  fct_diff_deriv=function(p_frame)
+  {
+    v_returned=c()
+    v_diff=diff(p_frame$amplitude)
+    v_returned=c(v_returned,0 )
+    for(i in 1:length(v_diff)-1)
+    {
+      v_returned=c(v_returned,v_diff[i+1]-v_diff[i] )
+    }
+    v_returned=c(v_returned,0 )
+    
+    p_frame$amplitude<-v_returned
+    
+    p_frame  
+  }
+  ,
+   #FREQUENCY FCT
+  eod_spectrum =function()
+  {
+    
+    
+    spec<-spectrum(padded_normalized_wave_for_periodgram$amplitude,fs= samplingRate , plot=FALSE)
+    
+    periodgram_frame <<-data.frame(freq=spec$freq,spec=spec$spec )
+    periodgram_frame$freq <<- periodgram_frame$freq * samplingRate
+    periodgram <<-ggplot(periodgram_frame, aes(x=periodgram_frame$freq, y=10*log10(periodgram_frame$spec/max(periodgram_frame$spec)) )) + geom_line() + scale_x_log10()
+    
+    
+  }
+  ,
+  pad_1_sec=function()
+  {
+    v_diff_1sec=1-max(normalized_wave$time)
+    print("diff 1 sec")
+    print(v_diff_1sec)
+    
+    v_time_interv=diff(normalized_wave$time)
+    v_mean_interval=mean(v_time_interv)
+    print("mean interv")
+    print(v_mean_interval)
+    #View(v_time_interv)
+    v_pad=seq(0,v_diff_1sec/2, by=v_mean_interval )
+    #View(pad)
+    #copy
+    v_1sec_frame=data.frame(normalized_wave)
+    v_1sec_frame$time<-v_1sec_frame$time + v_mean_interval+ (v_diff_1sec/2) 
+    v_pad_frame=data.frame(time=v_pad, amplitude=rep(0, times=length(v_pad)))
+    #View(v_pad_frame)
+    v_1sec_frame<-rbind(v_pad_frame,v_1sec_frame )
+    v_pad_frame$time=v_pad_frame$time + v_mean_interval +max(v_1sec_frame$time)
+    v_1sec_frame<-rbind(v_1sec_frame, v_pad_frame)
+    plot(v_1sec_frame, type='l')
+    print("Min padded")
+    print(min(v_1sec_frame$time))
+    print("Max padded")
+    print(max(v_1sec_frame$time))
+    padded_normalized_wave_for_periodgram <<-v_1sec_frame
+    
+    
+  },
+  createPeriodgram =function()
+  {
+    v_1sec_frame<-pad_1_sec()
+    v_plot<-eod_spectrum()  
+  },
+  #normalize time(private)
+  normalize_and_center_time=function(p_frame)
+  {
+    
+    #normalize time
+    p_frame$time<- p_frame$time / (max(p_frame$time) - min(p_frame$time))
+    #find center
+    max_pos = which(p_frame$amplitude ==(max(p_frame$amplitude)))
+    min_pos = which(p_frame$amplitude ==(min(p_frame$amplitude)))
+    interv<-c(max_pos, min_pos)
+    
+    print("Pos max and min")
+    print(interv)
+    
+    #if min before amx
+    interv_sorted<- sort(interv)
+    
+    print("Pos max and min - sorted")
+    print(interv_sorted)
+    print("p1 time")
+    print(p_frame[interv_sorted[1],1])
+    print("p2 time")
+    print(p_frame[interv_sorted[2],1])
+    
+    time_center_value = (p_frame[interv_sorted[2],1]+p_frame[interv_sorted[1],1]) /2
+    
+    
+    print("(p1 time + p2 time) / 2")
+    print(time_center_value)
+    
+    #closest array point
+    real_time_center_position = which(abs(p_frame$time - time_center_value)==min(abs(p_frame$time - time_center_value))) 
+    print("REAL TIME CENTER POSITION")
+    print(real_time_center_position)
+    real_time_center_value = p_frame[real_time_center_position, 1]
+    print("REAL TIME CENTER VALUE")
+    print(real_time_center_value)
+    
+    #substract time baseline value
+    p_frame$time <- p_frame$time - real_time_center_value
+    #return
+    print("return")
+    p_frame
+    
+  },
+  #GGPLOT
+  save_plot = function(p_frame, p_title, valleys_peaks, v_frame_zero, v_frame_start_end, v_chosen_baseline_position )
+  {
+    tmp_plot<-ggplot(p_frame,aes(x = time, y = amplitude)) + geom_line() + ggtitle(p_title)+
+      #geom_point(shape=8, data= v_frame_extrema , fill="black", color="black", size=3) + #display peaks
+      geom_point(shape=8, data= p_frame[valleys_peaks,] , fill="black", color="black", size=3) + #display peaks
+      #???geom_text(data= v_frame_extrema, label=v_frame_extrema$labels,  hjust=0,vjust=0) + #peaks label
+      geom_point(shape=16,   data= v_frame_zero ,  color="red", size=3) + #x=0 (halfway between peaks)
+      geom_point(shape=1,   data= v_frame_start_end ,  color="red", size=3) + #display start and stop
+      geom_text(data= v_frame_start_end, label=v_frame_start_end$labels,  hjust=0,vjust=0)+
+      geom_vline(aes(xintercept=p_frame$time[chosen_baseline_position]),
+                 color="red", linetype="dashed", size=1)
+    
+    tmp_plot
+  }
+  
+  
   
   )
 
--- eod_class_test.R
+++ eod_class_test.R
@@ -5,9 +5,11 @@ v <- Eod$new(specimenTag="test")
 
 print(v$getSpecimenTag())
 
-v$readFile("C:\\R_DEV_MORMY\\data_source\\MbisaCongo_MC-1006_A.csv")
+v$readFile("C:\\R_DEV_MORMY\\data_source\\MbisaCongo_MC-1013_A.csv")
 print(v$getSpecimenTag())
 View(v$getMetadata())
 View(v$getWave())
 View(v$getAbsoluteValues())
 View(v$getPossibleBaseline())
+print(v$getMainPlot())
+print(v$getDerivatePlot())
